6a13c0f44a6693945cda6c6e5dfbf5d554a0b6d0
unknown <phongken94@gmail.com>
2021-06-11 11:43:29 +0700

    day len

---
+++ src/main/java/com/example/demo/Movie.java
@@ -0,0 +1,30 @@
+package com.example.demo;
+
+public class Movie {
+    private String name;
+    private Integer score;
+
+    public Movie() {
+    }
+
+    public Movie(String name, Integer score) {
+        this.name = name;
+        this.score = score;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        this.score = score;
+    }
+}
